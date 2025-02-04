972662cb4ddcedaea669ed7eaaaaad1fe8fb61bf
pegifenu <110061199+pegifenu@users.noreply.github.com>
2025-02-03 20:22:03 -0800

    added accelerometer stuff

--- mqtt/serializers.py
+++ mqtt/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import Group, User
-from .models import Trip
+from .models import Trip, Accel
 from rest_framework import serializers
 
 
@@ -13,3 +13,8 @@ class TripSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Trip
         fields = ['id', 'name', 'date_created', 'start', 'stop']
+
+class AccelerometerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Accel
+        fields = ['id', 'trip', 'data', 'date']
